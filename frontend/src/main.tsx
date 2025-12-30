de7d78b7c620aae50bb9da2043aeb17282a2097a
vturovets <vturovets@gmail.com>
2025-12-30 10:26:56 +0200

    Revert "Global: reduce MUI Container gutters by 50%"

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 
 import App from "./App";
 import { queryClient } from "./config/queryClient";
-import { theme } from "./theme";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
